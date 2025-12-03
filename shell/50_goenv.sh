329fd5ebba7096951801867881f7a9b400901cfb
gary schaetz <gary@schaetzkc.com>
2025-12-03 15:30:41 -0600

    fixing various problems

--- shell/50_goenv.sh
+++ shell/50_goenv.sh
@@ -1,5 +1,6 @@
 if [[ "$DISABLE_GOENV" != "1" ]]; then
   if [[ -e $DOTFILES/thirdparty/goenv/bin/goenv ]]; then
+    export GOENV_PATH_ORDER=front
     export PATH=$DOTFILES/thirdparty/goenv/bin:$PATH
     eval "$(goenv init -)"
   fi
